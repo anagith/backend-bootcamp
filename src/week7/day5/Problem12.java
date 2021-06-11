d5033797d95c19e7f9186a08946ba73184bbf10c
Anahiit <amartirosyan904@gmail.com>
2021-06-11 18:21:23 +0400

    sort the array in ascending order,first by the number of certain value,then by the values themselves

--- src/week7/day5/Problem12.java
+++ src/week7/day5/Problem12.java
@@ -5,6 +5,7 @@ import java.util.*;
 
 
 public class Problem12 {
+
     public static void main(String[] args) {
         LinkedList<Integer> integers = new LinkedList<>();
         integers.add(5);
